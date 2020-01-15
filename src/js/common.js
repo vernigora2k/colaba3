29eacfea431c086bd71fec28d724463932930161
vernigora2k <58885741+vernigora2k@users.noreply.github.com>
2020-01-15 23:35:57 +0200

    added Babel. curent version v1.1

--- src/js/common.js
+++ src/js/common.js
@@ -1,2 +1,5 @@
 let add = (a,b) => a+b
 console.log(add(2,7))
+
+let add2 = (c,d) => c-d
+console.log(add(9,3))
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         publicPath: '/dist'
     },
+    module: {
+        rules: [
+            {
+             test: /\.js$/,
+             loader: 'babel-loader',
+             exclude: /node_modules/   
+            }
+        ]
+    },
 
     devServer: {
         overlay: true
